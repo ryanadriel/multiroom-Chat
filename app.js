f66a11b7ff7ba280240bcc37f5039027de54f37d
Adriel Ryan <adrielryan.tj@hotmail.com>
2021-09-14 18:57:36 -0300

    WebSocket communication

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 var app = require('./config/server');
 
-app.listen(80, function(){
+var server = app.listen(80, function(){
     console.log('Servidor online');
+})
+
+var io = require('socket.io').listen(server);
+
+io.on('connection', function(socket){
+    console.log('Usuário conectou');
+
+    socket.on('disconnect', function(){
+        console.log('Usuário desconectou');
+    })
 })
--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,16 @@
 module.exports.iniciaChat = function (application, req, res) {
-    res.render("chat");
+
+    var dadosForm = req.body;
+
+        req.assert('apelido', 'Nome ou apelido é obrigatório').notEmpty();
+        req.assert('apelido', 'Nome ou apelido deve conter entre 3 e 15 caracteres').len(3, 15);
+
+        var erros = req.validationErrors();
+
+        if(erros) {
+            res.render("index", { validacao : erros });
+            return;
+        }
+
+        res.render("chat");
 }
