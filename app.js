3b48bba0eb7293aa51ac4431642b2ad0920625a6
Adriel Ryan <adrielryan.tj@hotmail.com>
2021-09-15 12:00:58 -0300

    application completed

--- app.js
+++ app.js
@@ -6,10 +6,37 @@ var server = app.listen(80, function(){
 
 var io = require('socket.io').listen(server);
 
+app.set('io', io);
+
 io.on('connection', function(socket){
     console.log('Usuário conectou');
 
     socket.on('disconnect', function(){
         console.log('Usuário desconectou');
-    })
-})
+    });
+
+    socket.on('msgParaServidor', function(data) {
+        socket.emit(
+            'msgParaCliente',
+            { apelido: data.apelido, mensagem: data.mensagem }
+        );
+
+        socket.broadcast.emit(
+            'msgParaCliente',
+            { apelido: data.apelido, mensagem: data.mensagem }
+        );
+
+        if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+            socket.emit(
+                'participantesParaCliente',
+                { apelido: data.apelido }
+            );
+
+            socket.broadcast.emit(
+                'participantesParaCliente',
+                { apelido: data.apelido }
+            );
+        }
+    });
+
+});
